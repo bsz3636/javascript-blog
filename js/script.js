b62e2c9bf14582358afadefaeb3c9507cfaac280
Beata Karwowska <bsz3636@o2.pl>
2021-11-18 20:15:04 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });*/
-  const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
+  const titleClickHandler = function (event) {
+    const clickedElement = this;
+    console.log ('Link was clicked!');
+    console.log (event);
 
 /* remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
@@ -14,7 +15,8 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
   /* add class 'active' to the clicked link */
-
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .active');
 
